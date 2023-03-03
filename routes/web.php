e7f3e25825cdd705039816c14af95569cad99e1c
Ruben van Erk <ruben@rubenvanerk.nl>
2023-03-03 12:53:30 +0100

    Add read time to social image

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('social-image/{entry}', function ($entry) {
     $entry = \Statamic\Entries\Entry::find($entry);
 
-    $subtitle = $entry->seo_image_subtitle ?? ($entry->collection->handle === 'articles' ? 'Published on ' . $entry->date->format('M j, Y') : null);
+    $content = null;
+    $subtitle = $entry->seo_image_subtitle;
+
+    if ($entry->collection->handle === 'articles') {
+        $content = $entry->content;
+        $subtitle ??= 'Published on ' . $entry->date->format('M j, Y');
+    }
 
     return (new \Statamic\View\View)
         ->template('social_image')
         ->with([
             'title' => $entry->seo_image_title ?: $entry->title,
             'subtitle' => $subtitle,
+            'content' => $content,
         ]);
 })->name('social-image');
-
-//Route::statamic('social-image/{entry}', 'social_image');
